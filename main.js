83efa7ba1829b7a0ba1e647746321fd2b6e25249
Pietro <pietrolosciale9814gmail.com>
2026-04-10 18:17:55 +0200

    miglioramento UX:gestione dei post duplicati, migliorato feedback messaggi di successo/campo vuoto

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const postsContainer = document.getElementById('posts');
 
 let posts = [];
 
-//challenge 4:caricamento
+// challenge 4: caricamento
 
 postsContainer.innerHTML = "Caricamento...";
 
-//fetch dei dati dall'API,trasformazione in json e renderizzazione dei primi 5 post nella pagina
+// fetch dei dati dall'API, trasformazione in json e renderizzazione dei primi 5 post nella pagina
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then(response => response.json())
   .then(data => {
@@ -14,41 +14,36 @@ fetch("https://jsonplaceholder.typicode.com/posts")
     renderPosts(posts);
   })
 
-  //challenge 4: gestione errori, nel caso in cui la chiamata all'API non vada a buon fine. 
-  
+  // challenge 4: gestione errori, nel caso in cui la chiamata all'API non vada a buon fine. 
   .catch(error => {
     postsContainer.innerHTML = "Errore nel caricamento dei post";
     console.error(error);
   });
 
 
-  
 
-
-
-  //render e ciclo dei primi 5 post in home
+// render e ciclo dei primi 5 post in home
 function renderPosts(posts) {
   postsContainer.innerHTML = "";
 
-
-
-  //  aggiunta dinamica delle card per ogni post. 
+  // aggiunta dinamica delle card per ogni post. 
   posts.slice(0, 5).forEach(post => {
     postsContainer.innerHTML += `
       <div class="col-md-4">
 
-      <div class="card mb-5 mt-5 w-100">
-        <img src="https://picsum.photos/300/200" class="card-img-top w-100">
-        <div class="card-body">
-          <h5 class="card-title">${post.title}</h5>
-          <p class="card-text">${post.body}</p>
+        <div class="card mb-5 mt-5 w-100">
+          <img src="https://picsum.photos/300/200" class="card-img-top w-100">
+          <div class="card-body">
+            <h5 class="card-title">${post.title}</h5>
+            <p class="card-text">${post.body}</p>
+          </div>
         </div>
-      </div>
 
       </div>
     `;
   });
 }
+
 
 
 // logica di aggiunta di nuovi post
@@ -58,18 +53,45 @@ const form = document.getElementById('postForm');
 form.addEventListener('submit', function(e) {
   e.preventDefault();
 
+  // costanti per salvare i dati inseriti nel form, catturati da getElementById
+  const title = document.getElementById('title').value;
+  const body = document.getElementById('body').value;
 
-  // costanti per salvare i dati inseriti nel form, catturati da getElementById//
+  // elementi messaggi UI
+  const errorMessage = document.getElementById('errorMessage');
+  const successMessage = document.getElementById('successMessage');
+  const emptyMessage = document.getElementById('emptyMessage');
 
-   const title = document.getElementById('title').value;
-   const body = document.getElementById('body').value;
-  // console.log(title,body);
-
-  //gestione input vuoti, con alert
+  // gestione input vuoti- upgrade con alert al posto del popup del browser
   if (!title || !body) {
-  alert("Compila tutti i campi");
+  emptyMessage.classList.remove('d-none');
+
+  setTimeout(() => {
+    emptyMessage.classList.add('d-none');
+  }, 2000);
+
   return;
 }
+
+  // logica per evitare la creazione di post con titoli duplicati
+  const exists = posts.some(post => 
+    post.title.toLowerCase() === title.toLowerCase()
+  );
+
+  if (exists) {
+
+    // nasconde eventuale messaggio di successo
+    successMessage.classList.add('d-none');
+
+    // mostra errore-4sec
+    errorMessage.classList.remove('d-none');
+
+    setTimeout(() => {
+      errorMessage.classList.add('d-none');
+    }, 4000);
+
+    return;
+  }
 
   // creazione di un nuovo post con i dati inseriti nel form
   const newPost = {
@@ -81,20 +103,32 @@ form.addEventListener('submit', function(e) {
   // aggiunta del nuovo post IN CIMA alla lista dei post
   posts.unshift(newPost);
 
+  // mostra messaggio successo
+  errorMessage.classList.add('d-none');
+  successMessage.classList.remove('d-none');
+
+  setTimeout(() => {
+    successMessage.classList.add('d-none');
+  }, 2000);
+
+  // reset form
+  form.reset();
+
   // renderizzazione dei post aggiornati
   renderPosts(posts);
 });
 
 
+
 // logica di ricerca dei post
 const searchInput = document.getElementById('search');
 
-  searchInput.addEventListener('input', function() {
-      const value = searchInput.value.toLowerCase();
+searchInput.addEventListener('input', function() {
+  const value = searchInput.value.toLowerCase();
 
-      const filteredPosts = posts.filter(post =>
-        post.title.toLowerCase().includes(value)
-      );
+  const filteredPosts = posts.filter(post =>
+    post.title.toLowerCase().includes(value)
+  );
 
-    renderPosts(filteredPosts);
+  renderPosts(filteredPosts);
 });
